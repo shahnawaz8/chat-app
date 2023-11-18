06ee586a3a09df7665b7d7167aad799bdf4ea3cd
shahnawaz8 <shahnawaz846@gmail.com>
2023-11-18 17:53:22 +0530

    connected to the client and working with backend

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
+const connect = require('./db')
+const User = require('./user');
+const Chat = require('./chat');
 
 const app = express();
 const server = http.createServer(app);
@@ -17,7 +20,7 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-  console.log('A user connected');
+  console.log('A user connected',socket.id);
   
 
   // Handle events from the client
@@ -27,6 +30,13 @@ io.on('connection', (socket) => {
     // Broadcast the message to all connected clients
     io.emit('send message', msg);
   });
+  socket.on('message from', (msg) => {
+    console.log('Message: id' + msg);
+
+    // Broadcast the message to all connected clients
+    console.log('message come from'+ msg.message)
+    io.emit(msg.sender, msg.message);
+  });
 
   socket.on('disconnect', () => {
     console.log('User disconnected');
@@ -35,5 +45,6 @@ io.on('connection', (socket) => {
 
 
 server.listen(process.env.PORT || 8080, () => {
-  console.log('Server listening on http://localhost:8080');
+    connect();
+    console.log('Server listening on http:/c/localhost:8080');
 });
